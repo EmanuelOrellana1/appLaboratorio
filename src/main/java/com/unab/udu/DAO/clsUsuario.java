5ff350249e410f016e764153b283a54b2036fd00
MarcosMiranda00 <mm0014032019@unab.edu.sv>
2020-10-26 16:33:16 -0600

    cls

---
+++ src/main/java/com/unab/udu/DAO/clsUsuario.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.udu.DAO;
+
+import com.unab.edu.conexionbd.conexionbd;
+import com.unab.udu.Entidades.Usuario;
+import java.awt.List;
+import java.sql.Connection;
+
+/**
+ *
+ * @author PC
+ */
+public class clsUsuario {
+    
+    conexionbd con = new conexionbd();
+    Connection conectar = con.retornarConexion();
+    
+//    public List<Usuario> listaUsuario(String Usuario, String Contra, int Combo)
+//    {
+//    
+//    }
+    
+}
